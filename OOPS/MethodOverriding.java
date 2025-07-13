505626279b22193a943e742aa5bcbd369e06b3ce
Amarjeet Kumar <ahujajee2022@gmail.com>
2025-07-14 00:04:11 +0530

    method overrriding

---
+++ OOPS/MethodOverriding.java
@@ -0,0 +1,21 @@
+package OOPS;
+class X{
+    public void show(){
+        System.out.println("in superClass");
+    }   
+}
+class Y extends X{
+    public void show(){
+        System.out.println("in sub class");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Y y = new Y(); 
+        y.show(); 
+        // Y y1=new X(); // This line will cause a compile-time error because you cannot assign a superclass type to a subclass reference.
+        X x1=new Y(); //
+        x1.show(); // This will call the overridden method in class Y due to dynamic method dispatch.
+    }
+}
